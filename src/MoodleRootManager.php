496f72d0a72e602edde1a33388e16e2fbb01d014
Michael Aherne <micaherne@blueyonder.co.uk>
2024-03-18 23:10:35 +0000

    Added constructor checks.

--- src/MoodleRootManager.php
+++ src/MoodleRootManager.php
@@ -3,11 +3,19 @@
 namespace MoodlePhpstan;
 
 use Exception;
+use InvalidArgumentException;
 use MoodleAnalysis\Component\CoreComponentBridge;
 
 class MoodleRootManager
 {
-    public function __construct(private string $moodleRoot, private string $moodleVersion) {}
+    public function __construct(private string $moodleRoot, private string $moodleVersion) {
+        if (!is_dir($this->moodleRoot) || !file_exists($this->moodleRoot . '/lib/components.json')) {
+            throw new InvalidArgumentException("Moodle root does not exist or is not a valid Moodle codebase");
+        }
+        if (!is_dir($this->moodleRoot . '/vendor')) {
+            throw new Exception("Moodle must have vendor directory - please run composer install");
+        }
+    }
 
     public function createAliases(): void
     {
